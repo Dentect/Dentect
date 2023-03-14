df88246c69b017366a718f0362d7a6289e271a85
Tassneem04Hamdy <tassneemhamdy@gmail.com>
2023-03-14 04:06:40 +0200

    image storage and firebase configuration added

--- src/middlewares/imageStorage.ts
+++ src/middlewares/imageStorage.ts
@@ -1,14 +1,23 @@
+import express from 'express';
+import { ref, uploadBytesResumable, getDownloadURL } from 'firebase/storage';
 import multer from 'multer';
 import path from 'path';
+import storage from '../config/firebaseConfig';
 
-const storage = multer.diskStorage({
-    destination: function (_req, _file, cb) {
-        cb(null, './public/xrays');
-    },
-    filename: function (_req, file, cb) {
-        cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname));
+export const upload = multer({ storage: multer.memoryStorage() });
+
+export const uploadImage = async (req: express.Request, res: express.Response, next: express.NextFunction) => {
+    if (req.file === undefined) {
+        res.status(400).json('X-ray not found.');
+        return;
     }
-});
 
-const upload = multer({ storage: storage });
-export default upload;
+    const storageReference = ref(
+        storage,
+        `xrays/${path.parse(req.file.originalname).name}-${Date.now()}${path.parse(req.file.originalname).ext}`
+    );
+    const snapshot = await uploadBytesResumable(storageReference, req.file.buffer, { contentType: req.file.mimetype });
+
+    req.body.originalURL = await getDownloadURL(snapshot.ref);
+    next();
+};
