1335cf400fb7024ce0cae01cc9470c1c225a1dc3
Tassneem04Hamdy <tassneemhamdy@gmail.com>
2023-04-19 01:52:24 +0200

    edit routes

--- src/routes/dentistRoutes.ts
+++ src/routes/dentistRoutes.ts
@@ -1,11 +1,12 @@
 import express from 'express';
-// import validateToken from '../middlewares/validateToken';
+
+import validateToken from '../middlewares/validateToken';
 import { addPatient, getPatient, editPatient } from '../controllers/dentistControllers';
 
 const router = express.Router();
 
-router.post('/:dentistId/patients', addPatient);
-router.get('/:dentistId/patients/:patientName', getPatient);
-router.patch('/:dentistId/patients/:patientName', editPatient);
+router.post('/patients', validateToken, addPatient);
+router.get('/patients/:patientClinicId', validateToken, getPatient);
+router.patch('/patients/:patientClinicId', validateToken, editPatient);
 
 export default router;
--- src/routes/patientRoutes.ts
+++ src/routes/patientRoutes.ts
@@ -1,11 +1,12 @@
 import express from 'express';
-// import validateToken from '../middlewares/validateToken';
+
+import validateToken from '../middlewares/validateToken';
 import { upload, uploadImage } from '../middlewares/imageStorage';
 import { addPatientXray, getPatientXrays } from '../controllers/patientController';
 
 const router = express.Router();
 
-router.post('/:patientId/xrays', upload.single('xray'), uploadImage, addPatientXray);
-router.get('/:patientId/xrays', getPatientXrays);
+router.post('/:patientClinicId/xrays', validateToken, upload.single('xray'), uploadImage, addPatientXray);
+router.get('/:patientClinicId/xrays', validateToken, getPatientXrays);
 
 export default router;
