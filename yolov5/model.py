3d44e49e207168deb3e0bb7d1c5d23f241381bdc
Tassneem04Hamdy <tassneemhamdy@gmail.com>
2023-06-23 22:20:58 +0300

    model updates

--- yolov5/model.py
+++ yolov5/model.py
@@ -6,10 +6,9 @@ load_dotenv()
 
 def detectDiseases(imgPath):
     weightsPath = os.path.join(os.getenv('ROOT'),  'best.pt')
-    run(weights = weightsPath, source = imgPath, conf_thres = 0.1, imgsz = (416, 416))
+    run(weights = weightsPath, source = imgPath, conf_thres = 0.35, imgsz = (640,1280), iou_thres = 0.45)
     resultPath = os.path.join(os.getenv('ROOT') , 'yolov5', 'runs', 'detections', str(imgPath).split('xrays/')[1]) # Ubuntu
     # resultPath = os.path.join(os.getenv('ROOT') , 'yolov5', 'runs', 'detections', str(imgPath).split('xrays\\')[1]) # Windows
     return {
         'detectionURL': resultPath,
-        'report': 'xray.report'
     }
