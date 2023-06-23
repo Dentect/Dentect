3d5641b59cac49de9e8b1b6456da126b9bfa58e6
Tassneem04Hamdy <tassneemhamdy@gmail.com>
2023-06-23 23:33:47 +0300

    update OTP generation

--- src/helpers/generateOTP.ts
+++ src/helpers/generateOTP.ts
@@ -1,4 +1,4 @@
-import { google } from 'googleapis';
+// import { google } from 'googleapis';
 import nodemailer from 'nodemailer';
 import OTPGenerator from 'otp-generator';
 
@@ -38,28 +38,13 @@ const generateOTP = async (userEmail: string) => {
 
 
 const createTransporter = async () => {
-    const OAuth2Client = new google.auth.OAuth2(
-        process.env.CLIENT_ID,
-        process.env.CLIENT_SECRET,
-        'https://developers.google.com/oauthplayground'
-    );
-
-    OAuth2Client.setCredentials({
-        refresh_token: process.env.REFRESH_TOKEN
-    });
-
-    const accessToken = await OAuth2Client.getAccessToken() || '';
-
     const transporter = nodemailer.createTransport({
-        service: 'gmail',
+        host: 'smtp.gmail.com',
+        port: 465,
         auth: {
-            type: 'OAuth2',
             user: process.env.EMAIL,
-            accessToken: accessToken.token || '',
-            clientId: process.env.CLIENT_ID,
-            clientSecret: process.env.CLIENT_SECRET,
-            refreshToken: process.env.REFRESH_TOKEN
-        }
+            pass: process.env.PASSWORD,
+        },
     });
 
     return transporter;
